084b520bc3b1649712f69aaf9b5626a41682f35f
headshot <nikheel.patelce@gmail.com>
2022-04-10 23:38:25 +0530

    commit modifier

---
+++ commit.bat
@@ -0,0 +1,6 @@
+set GIT_AUTHOR_NAME=headshot
+set GIT_AUTHOR_EMAIL=nikheel.patelce@gmail.com
+set GIT_COMMITTER_NAME=headshot
+set GIT_COMMITTER_EMAIL=nikheel.patelce@gmail.com
+
+git commit -m "commit modifier"
